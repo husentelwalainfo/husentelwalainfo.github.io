53a007c4538817346b3671313e1172964f01d18d
husenindia <husentelwala@gmail.com>
2022-01-08 12:16:56 +0530

    About resume and contact page component added

--- newsite/src/app/app.module.ts
+++ newsite/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
